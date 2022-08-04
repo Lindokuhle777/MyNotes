e9f807cf324592fcd9de8ffa445f52e6dc9ab9f3
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-08-05 01:02:30 +0200

    some Changes

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,13 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "@firebase/storage";
+import dotenv from "dotenv";
+
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsCu6ZweHMsX6zkDKIUDLMdMgzZD7jDbA",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "my-notes-92a9f.firebaseapp.com",
   projectId: "my-notes-92a9f",
   storageBucket: "my-notes-92a9f.appspot.com",
