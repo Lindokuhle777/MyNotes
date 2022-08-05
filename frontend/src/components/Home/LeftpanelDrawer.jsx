2ff6c37e8a494378c1412e8e49aba41201cb67fc
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-08-05 03:25:51 +0200

    poper fixed

--- frontend/src/components/Home/LeftpanelDrawer.jsx
+++ frontend/src/components/Home/LeftpanelDrawer.jsx
@@ -1,4 +1,4 @@
-import React,{ useContext} from "react";
+import React, { useContext } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -15,7 +15,7 @@ import { uid } from "uid";
 import { Paper, TextField, Typography } from "@mui/material";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import {HomeContext} from "./HomeContext";
+import { HomeContext } from "./HomeContext";
 import { AuthContext } from "../../MainContext";
 import axios from "axios";
 
@@ -37,7 +37,14 @@ const btnSty = {
 };
 
 export default function TemporaryDrawer() {
-  const {openDrawer,handleCloseDrawer,collections, setCollections, setCurrCollection,getCollections} = React.useContext(HomeContext);
+  const {
+    openDrawer,
+    handleCloseDrawer,
+    collections,
+    setCollections,
+    setCurrCollection,
+    getCollections,
+  } = React.useContext(HomeContext);
   const { user } = useContext(AuthContext);
   const [open, setOpen] = React.useState(false);
   const anchor = "left";
@@ -45,7 +52,6 @@ export default function TemporaryDrawer() {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
     setOpen(true);
   };
 
@@ -74,90 +80,93 @@ export default function TemporaryDrawer() {
     }
     setOpen(false);
   };
-  
 
   return (
     <div>
       <React.Fragment>
-        <Drawer
-          anchor={anchor}
-          open={openDrawer}
-          onClose={handleCloseDrawer}
-        >
+        <Drawer anchor={anchor} open={openDrawer} onClose={handleCloseDrawer}>
           <div style={mainDiv}>
-      <Typography variant="h4" style={{ color: "white" }}>
-        Collections
-      </Typography>
-      <Button
-        size="large"
-        variant="filled"
-        style={btnSty}
-        onClick={handleClick}
-      >
-        New Collection
-      </Button>
-      <div>
-        <List
-          dense={true}
-          style={{
-            maxHeight: "350px",
-            overflowY: "auto",
-            margin: "10% 5%",
-            borderRadius: "10px",
-          }}
-        >
-          {collections.map((item, index) => (
-            <div key={item.uuid}>
-              <ListItem
-                onClick={(event) => {
-                  event.preventDefault();
-                  setCurrCollection(item);
-                }}
-                button
-                style={{
-                  backgroundColor: "white",
-                  borderRadius: "10px",
-                  marginBottom: "5px",
-                }}
-                secondaryAction={
-                  <IconButton edge="end" aria-label="delete">
-                    <DeleteIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={item.name} />
-              </ListItem>
-            </div>
-          ))}
-        </List>
-      </div>
-      <Popper id={id} open={open} anchorEl={anchorEl} transition>
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
-            <Paper
-              style={{ padding: "10px", display: "flex", flexDirection: "row" }}
+            <Typography variant="h4" style={{ color: "white" }}>
+              Collections
+            </Typography>
+
+            <Button
+              size="large"
+              variant="filled"
+              style={btnSty}
+              onClick={handleClick}
             >
-              <TextField
-                variant="standard"
-                placeholder="Collection Name"
-                id="collectionName"
-                size="small"
-              />
-              <Button size="small" onClick={handleNewCollection}>
-                Save
-              </Button>
-            </Paper>
-          </Fade>
-        )}
-      </Popper>
-    </div>
+              New Collection
+            </Button>
+            {open && (
+              <Paper
+                style={{
+                  padding: "10px",
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "80%",
+                  position: "absolute",
+                  left: 0,
+                  right: 0,
+                  margin: "auto",
+                }}
+              >
+                <TextField
+                  variant="standard"
+                  placeholder="Collection Name"
+                  id="collectionName"
+                  size="small"
+                />
+                <Button size="small" onClick={handleNewCollection}>
+                  Save
+                </Button>
+              </Paper>
+            )}
+
+            <div>
+              <List
+                dense={true}
+                style={{
+                  maxHeight: "350px",
+                  overflowY: "auto",
+                  margin: "15% 5%",
+                  borderRadius: "10px",
+                }}
+              >
+                {collections.map((item, index) => (
+                  <div key={item.uuid}>
+                    <ListItem
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCurrCollection(item);
+                      }}
+                      button
+                      style={{
+                        backgroundColor: "white",
+                        borderRadius: "10px",
+                        marginBottom: "5px",
+                      }}
+                      secondaryAction={
+                        <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      }
+                    >
+                      <ListItemAvatar>
+                        <Avatar>
+                          <FolderIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary={item.name} />
+                    </ListItem>
+                  </div>
+                ))}
+              </List>
+            </div>
+          </div>
         </Drawer>
       </React.Fragment>
+      
     </div>
   );
 }
